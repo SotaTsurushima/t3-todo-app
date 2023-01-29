6d26eb8ddcc6f22cbc8eff5ec8ef0b92c3b8e41f
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-01-29 17:14:06 +0900

    add validation file

---
+++ src/pages/api/schema/todo.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+// for create
+export const createTaskSchema = z.object({
+  title: z.string().max(20),
+  body: z.string().min(5),
+})
+
+export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>
+
+// for update
+export const updateTaskSchema = z.object({
+  taskId: z.string().cuid(),
+  title: z.string().max(20),
+  body: z.string().min(5),
+})
+
+export type UpdateTaskInput = z.TypeOf<typeof updateTaskSchema>
+
+export const getSingleTaskSchema = z.object({
+  taskId: z.string().cuid(),
+})
