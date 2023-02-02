4caa89679c105297757b592f11bfb21f80ddb5f7
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-02-02 22:33:15 +0900

    customized validation

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,12 +1,21 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
-
 import { type Context } from "./context";
+import { ZodError } from 'zod'
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST' && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
   },
 });
 
